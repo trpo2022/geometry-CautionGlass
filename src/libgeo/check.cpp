6a556047c2f775a3dd47171a51b5d877c2130c57
Ivan Kharchenko <Cautionglass@users.noreply.github.com>
2022-03-28 19:41:28 +0700

    fix src

--- src/libgeo/check.cpp
+++ src/libgeo/check.cpp
@@ -4,14 +4,25 @@
 
 using namespace std;
 
-void check(float x0, float y0, float r, float x1, float y1, float x2, float y2, float x3, float y3)
+void check(
+        float x0,
+        float y0,
+        float r,
+        float x1,
+        float y1,
+        float x2,
+        float y2,
+        float x3,
+        float y3)
 {
-	if (((sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1))>r) && (sqrt((x0-x2)*(x0-x2)+(y0-y2)*(y0-y2))>r) && (sqrt((x0-x3)*(x0-x3)+(y0-y3)*(y0-y3))>r)) || ((sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1))<r) && (sqrt((x0-x2)*(x0-x2)+(y0-y2)*(y0-y2))<r) && (sqrt((x0-x3)*(x0-x3)+(y0-y3)*(y0-y3))<r)))
-	{
-		cout << "don 't overlap" << endl;
-	}
-	else
-	{
-		cout << "overlap" << endl;
-	}
-} 
+    if (((sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)) > r)
+         && (sqrt((x0 - x2) * (x0 - x2) + (y0 - y2) * (y0 - y2)) > r)
+         && (sqrt((x0 - x3) * (x0 - x3) + (y0 - y3) * (y0 - y3)) > r))
+        || ((sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)) < r)
+            && (sqrt((x0 - x2) * (x0 - x2) + (y0 - y2) * (y0 - y2)) < r)
+            && (sqrt((x0 - x3) * (x0 - x3) + (y0 - y3) * (y0 - y3)) < r))) {
+        cout << "don 't overlap" << endl;
+    } else {
+        cout << "overlap" << endl;
+    }
+}
